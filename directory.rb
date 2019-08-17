aafba981c7a677f901a69ce5e2dab5d5a36cf3a2
Julian George <jules104@gmail.com>
2019-08-17 15:02:03 +0100

    Add method to take load students request from command line on startup

--- directory.rb
+++ directory.rb
@@ -1,39 +1,18 @@
 @students = [] #an empty array accessible to all methods
 
-def input_students
-    puts "please enter the names of the students"
-    puts "To finish, just hit return twice"
-    # get the first name
-    name = gets.chomp
-    # while name is not empty repeat this code
-    while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    # get anotehr name from the user
-    name = gets.chomp
-  end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
-end
-
-def print_menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. Save the list to students.csv"
-    puts "4. Load the list from students.csv"
-    puts "9. Exit" # 9 because we'll be adding more items
-end
-
-def show_students
-    print_header
-    print_student_list
-    print_footer
 end
 
 def process(selection)
@@ -53,9 +32,30 @@ def process(selection)
   end
 end
 
+def input_students
+  puts "please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while name is not empty repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get anotehr name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
 def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def print_student_list
@@ -80,18 +80,26 @@ def save_students
   file.close
 end
 
-def load_students
-    file = File.open("students.csv", "r")
-    file.readlines.each do |line|
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-      @students << {name: name, cohort: cohort.to_sym}
-    end
-    file.close
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
-#nothing happens until we call the methods
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
+try_load_students
 interactive_menu
-#students = input_students
-#print_header
-#print(students)
-#print_footer(students)
